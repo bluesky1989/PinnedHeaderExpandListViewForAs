b425ec844e8cacea4f75f0af6aa240ec9dd284cf
duyongqiang <yq03130917>
2019-07-19 10:42:04 +0800

    first commit

---
+++ app/src/main/java/com/dyq/pinnedheaderexpandlistviewforas/entity/Group.java
@@ -0,0 +1,20 @@
+package com.dyq.pinnedheaderexpandlistviewforas.entity;
+
+/**
+ * author:duyongqiang
+ * mail: duyongqiang09@126.com
+ * date:2019/7/16
+ * Description:
+ */
+public class Group {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ app/src/main/java/com/dyq/pinnedheaderexpandlistviewforas/entity/People.java
@@ -0,0 +1,39 @@
+package com.dyq.pinnedheaderexpandlistviewforas.entity;
+
+/**
+ * author:duyongqiang
+ * mail: duyongqiang09@126.com
+ * date:2019/7/16
+ * Description:
+ */
+public class People {
+
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
